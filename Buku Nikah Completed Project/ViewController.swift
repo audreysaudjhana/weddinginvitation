76c3ca76fac33376e028d2f5a2ab6487e263c035
Audrey Saudjhana <56724687+audreysaudjhana@users.noreply.github.com>
2020-08-06 16:27:43 +0700

    First Upload

---
+++ Buku Nikah Completed Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Buku Nikah Completed Project
+//
+//  Created by Muhammad Irfan on 06/08/20.
+//  Copyright © 2020 Apalo13. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
